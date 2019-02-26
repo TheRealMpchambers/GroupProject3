3fa45d0f412fde898d1d702f3712680b8b93ae95
pkeane92 <pkeane92@yahoo.com>
2019-02-25 22:21:34 -0600

    created room model and started working on api routes

---
+++ models/rooms.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    roomNumber: {type: String, required: true},
+    floor: {type: Number},
+    map: {type: String, required: true},
+    directions: {type: [String], required: true}
+});
+
+const Room = mongoose.model("Room", roomSchema);
+
+module.exports = Room;
---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.route("/")
+.get(
+    db.Room.find({})
+    .then(dbModel => res.json(dbModel))
+    .catch(err => console.log(err))
+    )
+.post(
+    db.Room.create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => console.log(err))
+    )
