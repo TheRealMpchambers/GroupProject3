badfcba0bafa76159b50d2ebf985ffc7a965b7b1
Walt Disney <wyattreiman@Wyatts-MacBook-Pro-2.local>
2019-03-13 18:04:48 -0500

    added routes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,6 +7,7 @@ import Results from "../Results/Results";
 import PresetChoices from "../PresetChoices/PresetChoices";
 import API from '../../utils/API';
 import { withRouter } from "react-router";
+import { Button } from 'react-bootstrap';
 
 class Home extends Component {
 
@@ -17,12 +18,12 @@ class Home extends Component {
             searchInput: "",
             results: ""
         };
-     
+
     }
 
-     //makes sure something is entered in the form fields
+    //makes sure something is entered in the form fields
 
-     validateForm() {
+    validateForm() {
         return this.state.searchInput.length > 0;
     }
 
@@ -33,20 +34,21 @@ class Home extends Component {
             [event.target.id]: event.target.value
         });
     }
-    
+
     handleSubmit = event => {
         event.preventDefault();
         console.log(this.state.searchInput);
         var query = this.state.searchInput;
         if ((query.indexOf(" ")) > -1) {
             query = query.replace(/\s/g, "_")
-      };
+        };
         console.log("The query is " + query);
         API.getDirections(query)
-        .then(res => {
-            this.setState({
-                results: res.data})
-        })
+            .then(res => {
+                this.setState({
+                    results: res.data
+                })
+            })
 
     }
 
@@ -60,12 +62,12 @@ class Home extends Component {
             query = query.replace(/\'/g, "")
         }
         API.getDirections(query)
-        .then(res => {
-            console.log(res.data)
-            this.setState({
-                results: res.data
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    results: res.data
+                })
             })
-        })
         this.props.history.push("/Homes");
     }
 
@@ -73,19 +75,50 @@ class Home extends Component {
         console.log(this.state)
     }
 
-    render () {
+    render() {
+        // const isAuthenticated = this.auth.isAuthenticated;
 
         return (
             <div className="App">
                 <Header1 />
-                <button onClick={this.props.auth}>Admin Login</button>
-                <RoomSearch 
-                handleSubmit = {this.handleSubmit}
-                searchInput = {this.state.searchInput}
-                handleChange = {this.handleChange}
-                validateForm = {this.validateForm}/>
+
+            {/* //     <Button */}
+            {/* //         bsStyle="primary"
+            //         className="btn-margin"
+            //         onClick={this.goTo.bind(this, 'home')}
+            //     >
+            //         Home
+            // </Button> */}
+            {/* //     { */}
+            {/* //         !isAuthenticated() && (
+            //             <Button */}
+            {/* //                 bsStyle="primary"
+            //                 className="btn-margin"
+            //                 onClick={this.login.bind(this)}
+            //             >
+            //                 Log In
+            //       </Button>
+            //         ) */}
+            {/* //     }
+            //     { */}
+            {/* //         isAuthenticated() && (
+            //             <Button
+            //                 bsStyle="primary"
+            //                 className="btn-margin"
+            //                 onClick={this.logout.bind(this)}
+            //             >
+            //                 Log Out
+            //       </Button>
+            //         )
+            //     } */}
+                {/* <button onClick={this.props.auth}>Admin Login</button> */}
+                <RoomSearch
+                    handleSubmit={this.handleSubmit}
+                    searchInput={this.state.searchInput}
+                    handleChange={this.handleChange}
+                    validateForm={this.validateForm} />
                 <PresetChoices
-                presetSubmit = {this.presetSubmit}/>
+                    presetSubmit={this.presetSubmit} />
                 {/* <div>
                 <br></br><br></br><br></br><br></br><br></br><br></br>
                 <Header2/>
